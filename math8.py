1b883f00cc9458a4f625a421384cc5db7036beae
vido8 <vidyasr90@gmail.com>
2025-08-12 11:21:04 +0530

    Add files via upload

---
+++ math8.py
@@ -0,0 +1,17 @@
+import math
+n= int(input("enter a number: "))
+print(math.sqrt(25))
+
+print(f"square root is: {math.sqrt(25)}")
+print(math.log(25))
+
+print(f"logarithm is: {math.log(25)}")
+print(math.sin(25))
+print(f"sine is: {math.sin(25)}")
+
+
+
+
+
+
+
